a01bebd8a4f6a9408f3e77a1dd2db1015744a87a
Bruzzone Marcelo Daniel <bruzzone1804@gmail.com>
2023-06-01 00:43:58 -0300

    primer commit

---
+++ cunumi/webapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home,name=''),
+    path('register',views.register, name='register'),
+    path('my-login',views.my_login, name='my-login'),
+    path('user-logout',views.user_logout, name='user-logout'),
+    path('dashboard',views.dashboard, name='dashboard'),    
+]
